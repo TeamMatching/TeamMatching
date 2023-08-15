1b4faa9a1277db8be27970d4b153826d16bbcd6c
sanikani <sanikani@naver.com>
2023-08-16 04:33:11 +0900

    bootstrap 활용 main 페이지 추가

--- src/main/java/teammatching/teammatching/Controller.java
+++ src/main/java/teammatching/teammatching/Controller.java
@@ -1,11 +1,9 @@
 package teammatching.teammatching;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @org.springframework.stereotype.Controller
-@RequestMapping("/TeamMatching")
+@RequestMapping("/team-matching")
 public class Controller {
 
     @GetMapping //메인 페이지
@@ -16,7 +14,7 @@ public class Controller {
     @GetMapping("/posts/{id}") //게시글 상세조회
     public String postDetail(@PathVariable Integer id) {
 
-        return "post_detail";
+        return "post-detail";
     }
 
     @GetMapping("/add") //게시글 등록 폼
@@ -27,12 +25,12 @@ public class Controller {
     @PostMapping("/add")
     public String addPost() {
 
-        return "redirect:/TeamMatching";
+        return "redirect:/team-matching";
     }
 
     @DeleteMapping("/posts/delete/{id}")
     public String deletePost(@PathVariable Integer id) {
-        return "redirect:/TeamMatching";
+        return "redirect:/team-matching";
     }
 
     @GetMapping("/posts/{id}/edit")
